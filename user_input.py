6708ee1cb35eec1cc24575dc3c57ffca74fdd50f
Riranzu <riranzukhosa4@gmail.com>
2024-03-05 10:18:50 +0200

    update README

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name= input('what is your name?')
+age=input('what is your age?')
+location= input('where do you reside?')
+
+print('Hello ', name, ', you are ', age, ' years old and you reside in ', location )
